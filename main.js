28f2cf22d80d9d12ad000a477d8f99711dfd6bf0
Malkit Singh <malkitsingh01@gmail.com>
2020-08-14 23:54:24 +0530

    sync

--- main.js
+++ main.js
@@ -33,49 +33,89 @@ function sendSubscription(subscription) {
 }
 
 function subscribeUser() {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready
-      .then(function (registration) {
-        if (!registration.pushManager) {
-          console.log("Push manager unavailable.");
-          return;
-        }
+  console.log("came in here");
 
-        registration.pushManager
-          .getSubscription()
-          .then(function (existedSubscription) {
-            if (existedSubscription === null) {
-              console.log("No subscription detected, make a request.");
-              registration.pushManager
-                .subscribe({
-                  applicationServerKey: convertedVapidKey,
-                  userVisibleOnly: true,
-                })
-                .then(function (newSubscription) {
-                  console.log("New subscription added.");
-                  sendSubscription(newSubscription);
-                })
-                .catch(function (e) {
-                  if (Notification.permission !== "granted") {
-                    console.log("Permission was not granted.");
-                  } else {
-                    console.error(
-                      "An error ocurred during the subscription process.",
-                      e
-                    );
-                  }
-                });
-            } else {
-              console.log("Existed subscription detected.");
-              sendSubscription(existedSubscription);
-            }
-          });
-      })
-      .catch(function (e) {
-        console.error(
-          "An error ocurred during Service Worker registration.",
-          e
-        );
-      });
-  }
+  //   if ("serviceWorker" in navigator) {
+  //     console.log('came in here 2');
+  //     navigator.serviceWorker.ready
+  //       .then(function (registration) {
+  //         console.log('came in here 3');
+  //         if (!registration.pushManager) {
+  //           console.log("Push manager unavailable.");
+  //           return;
+  //         }
+
+  //         registration.pushManager
+  //           .getSubscription()
+  //           .then(function (existedSubscription) {
+  //             if (existedSubscription === null) {
+  //               console.log("No subscription detected, make a request.");
+  //               registration.pushManager
+  //                 .subscribe({
+  //                   applicationServerKey: convertedVapidKey,
+  //                   userVisibleOnly: true,
+  //                 })
+  //                 .then(function (newSubscription) {
+  //                   console.log("New subscription added.");
+  //                   sendSubscription(newSubscription);
+  //                 })
+  //                 .catch(function (e) {
+  //                   if (Notification.permission !== "granted") {
+  //                     console.log("Permission was not granted.");
+  //                   } else {
+  //                     console.error(
+  //                       "An error ocurred during the subscription process.",
+  //                       e
+  //                     );
+  //                   }
+  //                 });
+  //             } else {
+  //               console.log("Existed subscription detected.");
+  //               sendSubscription(existedSubscription);
+  //             }
+  //           });
+  //       })
+  //       .catch(function (e) {
+  //         console.error(
+  //           "An error ocurred during Service Worker registration.",
+  //           e
+  //         );
+  //       });
+  //   }
+  //   else{
+  //       console.log('came in else');
+
+  //   }
+}
+
+if ("serviceWorker" in navigator) {
+  console.log("Registering service worker");
+
+  run().catch((error) => console.error(error));
+}
+
+async function run() {
+  console.log("Registering service worker");
+  const registration = await navigator.serviceWorker.register("/sw.js", {
+    scope: "/",
+  });
+  console.log("Registered service worker");
+
+  console.log("Registering push");
+  const subscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+  });
+  console.log("Registered push");
+
+  console.log("Sending push");
+  sendSubscription(subscription)
+//   await fetch("/subscribe", {
+//     method: "POST",
+//     body: JSON.stringify(subscription),
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//   });
+  console.log("Sent subscription");
 }
--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-self.addEventListener("push", (event) => {
-  const data = event.data.json();
-  console.log("New notification", data);
-  const options = {
-    body: data.body,
-  };
-  event.waitUntil(self.registration.showNotification(data.title, options));
+console.log('Loaded service worker!');
+
+self.addEventListener('push', ev => {
+  const data = ev.data.json();
+  console.log('Got push', data);
+  self.registration.showNotification(data.title, {
+    body: 'Hello, World!',
+    icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
+  });
 });
